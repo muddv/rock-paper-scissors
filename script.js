f2c057ab2085b800a08a3f826193c3b578599318
misha <dedovmu@gmail.com>
2022-04-13 13:28:26 +0300

    This project is basically complete, some code clean up is in order though Page design is ugly, but that is what this game needs

--- script.js
+++ script.js
@@ -38,75 +38,85 @@ function playerSelection() {
 function playRound(computerPlay, playerSelection) {
     // case: tie
     if (computerPlay === playerSelection) {
-        console.log("Tie!");
-        displayResults("Tie!", playerWin, computerWin);
+        pCheck = 0;
+        cCheck = 0;
+        displayResults("Tie!", playerWin, computerWin, pCheck, cCheck);
     }
     // conditions for different cases
     else {
         if (computerPlay === "ROCK") {
             if (playerSelection === "PAPER") {
-                console.log("You win! Paper beats rock!");
                 playerWin += 1;
-                displayResults("You win! Paper beats rock!", playerWin, computerWin);
+                pCheck = 1;
+                cCheck = 0;
+                displayResults("You win! Paper beats rock!", playerWin, computerWin, pCheck, cCheck);
             }
             else if (playerSelection === "SCISSORS") {
-                console.log("You loose! Rock beats scissors");
                 computerWin += 1;
-                displayResults("You loose! Rock beats scissors", playerWin, computerWin);
+                pCheck = 0;
+                cCheck = 1;
+                displayResults("You loose! Rock beats scissors", playerWin, computerWin, pCheck, cCheck);
             }
         }
         if (computerPlay === "PAPER")
         {
             if (playerSelection === "SCISSORS") {
-                console.log("You win! Scissors beat rock");
                 playerWin += 1;
-                displayResults("You win! Scissors beat rock", playerWin, computerWin);
+                pCheck = 1;
+                cCheck = 0;
+                displayResults("You win! Scissors beat rock", playerWin, computerWin, pCheck, cCheck);
             }
             else if (playerSelection === "ROCK") {
-                console.log("You loose! Paper beats rock");
                 computerWin += 1;
-                displayResults("You loose! Paper beats rock", playerWin, computerWin);
+                pCheck = 0;
+                cCheck = 1;
+                displayResults("You loose! Paper beats rock", playerWin, computerWin, pCheck, cCheck);
             }
         }
         if (computerPlay === "SCISSORS") {
             if (playerSelection === "ROCK") {
                 console.log("You win! Rock beats scissors!");
                 playerWin += 1;
-                displayResults("You win! Rock beats scissors!", playerWin, computerWin);
+                pCheck = 1;
+                cCheck = 0;
+                displayResults("You win! Rock beats scissors!", playerWin, computerWin, pCheck, cCheck);
             }
             else if (playerSelection === "PAPER") {
                 console.log("You loose! Scissors beat paper!");
                 computerWin += 1;
-                displayResults("You loose! Scissors beat paper!", playerWin, computerWin);
+                pCheck = 0;
+                cCheck = 1;
+                displayResults("You loose! Scissors beat paper!", playerWin, computerWin, pCheck, cCheck);
             }
         }
     }
 }
 
-function displayResults(text, playerResult, computerResult) {
+function displayResults(text, playerResult, computerResult, pCheck, cCheck) {
         const resultText = document.querySelector("#resultText")
         resultText.textContent = text    
         
-        const playerScore = document.querySelector("#playerScore")
+        const playerScore = document.querySelector("#pScore")
         playerScore.textContent = playerResult
 
-        const computerScore = document.querySelector("#computerScore")
+        const computerScore = document.querySelector("#cScore")
         computerScore.textContent = computerResult
 
-        if (playerResult > computerResult) {
+        if (pCheck === 1) {
             document.querySelectorAll("img")[0].src = "images/player_win.jpeg"
             document.querySelectorAll("img")[1].src = "images/computer_lose.jpeg"
         }
 
-        if (playerResult === computerResult) {
+        if (cCheck === 1) {
+            document.querySelectorAll("img")[0].src = "images/player_lose.jpeg"
+            document.querySelectorAll("img")[1].src = "images/computer_win.jpeg"
+        }
+
+        if ((pCheck === 0) && (cCheck === 0)) {
             document.querySelectorAll("img")[0].src = "images/player_tie.jpeg"
             document.querySelectorAll("img")[1].src = "images/computer_tie.jpeg"
         }
 
-        if (computerResult > playerResult) {
-            document.querySelectorAll("img")[0].src = "images/player_lose.jpeg"
-            document.querySelectorAll("img")[1].src = "images/computer_win.jpeg"
-        }
 
         if (playerWin > 5) {
             playerWin = 0
